b20b3ec80d1a7c8e27f10f81fcd7940c0f61da6d
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2022-02-23 18:52:11 -0300

    criação dos arquivos de configuração do Terraform necessários para provisionar uma instância EC2 na AWS em São Paulo

---
+++ aws-instance/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-090006f29ecb2d79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
